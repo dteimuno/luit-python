bdfde59569f7140115dc899947e10b6b629e289b
Dennis Tei-Muno <munodennis@gmail.com>
2024-10-15 03:34:26 -0500

    Create module1-more-about-print.py

---
+++ module1-more-about-print.py
@@ -0,0 +1,7 @@
+print('Hello')
+print("I'm studying python")
+print('I\'m studying  Python')
+print("I'm studying Python")
+print("and I'm having fun")
+print("I'm studying Python", "and having fun")
+print("I'm studying Python \nand having  fun")
