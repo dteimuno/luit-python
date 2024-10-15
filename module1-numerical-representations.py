3ce3cde1f74d134e085210c3cd9ef61c71b3172d
Dennis Tei-Muno <munodennis@gmail.com>
2024-10-15 04:08:25 -0500

    Create module1-numerical-representations.py

---
+++ module1-numerical-representations.py
@@ -0,0 +1,14 @@
+#underscores in numbers
+12000300
+12_000_300
+
+#scientific notation
+#3e4 = 3E4 = 3 * 10000 = 30000
+print(0.0000005)
+
+#octal numbers
+#start with 0O or 0o
+print(0o123)
+
+#hexadecimal numbers
+#start with 0X or 0x
