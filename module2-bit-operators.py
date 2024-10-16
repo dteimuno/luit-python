ca9f82a54da9946e9783842721aa43ca4c446f7a
Dennis Tei-Muno <munodennis@gmail.com>
2024-10-16 03:20:55 -0500

    Create module2-bit-operators.py

---
+++ module2-bit-operators.py
@@ -0,0 +1,21 @@
+first_bit = 1
+second_bit = 1
+print(first_bit & second_bit)
+
+#logical Or
+first_bit = 0
+second_bit = 0
+print(first_bit | second_bit)
+
+# ^ exclusive OR
+first_bit = 0
+second_bit = 0
+print(first_bit ^ second_bit)
+
+# ~logical NOT
+print(~0)
+
+#long shift
+print(12 << 1)
+
+
