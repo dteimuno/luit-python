b71d5581046b1886876ccc4af4a0e0d437d4962b
Dennis Tei-Muno <munodennis@gmail.com>
2024-10-21 16:31:58 -0500

    Create module4-intro-lists.py

---
+++ module3/module4-intro-lists.py
@@ -0,0 +1,9 @@
+top_cities = ['New York City', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix']
+print(top_cities[0])
+
+print(top_cities[-1])
+print(top_cities[-2])
+print(top_cities[0:2])
+print(top_cities[2:])
+print(top_cities[:3])
+print(top_cities[0:1])
